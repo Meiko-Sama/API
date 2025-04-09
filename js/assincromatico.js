13f0371bb59636090c91a17a85737ca83f5bf9d5
Aluno <thayna.matos.carlos@gmail.com>
2025-04-09 08:22:33 -0300

    Sincronização da API

--- js/assincromatico.js
+++ js/assincromatico.js
@@ -1,3 +1,12 @@
+// API -> Ela fornece dados
+// PROMISSE ->  Ela vai devolver alguma coisa, promessa que vai ter uma resposta,
+// pode ser de erro, de sucesso, que falhou ou ficar esperando até receber uma resposta da API
+// FETCH -> Funções do método HTTP
+// ASYNC -> Significa que a função ela é assincronas, quando o botao 1
+// for acionado ele vai demorar para pegar dados, mas ele vai continuar
+// apertando os outros e mandando resposta mesmo que o botao 1 ainda esteja carregando
+// a resposta, quando ele enviar não vai mudar nada.
+
 // COMO UMA API FUNCIONA??
 
 // const minhaPromise = new Promise((resolve, reject) => {
@@ -52,16 +61,101 @@
 
 // ASYNC/AWAIT - SEGUNDA MANEIRA DE MANDAR API
 
-async function buscarEndereco(cep) {
-  try {
-    console.log("Buscando endereço...");
-    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const dados = await resposta.json();
-    console.log(dados);
-  } catch (error) {
-    console.log(error);
+// async function buscarEndereco(cep) {
+//   try {
+//     console.log("Buscando endereço...");
+//     const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+//     const dados = await resposta.json();
+//     console.log(dados);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// buscarEndereco("01001000");
+// console.log("Requisição Enviada");
+
+class Endereco {
+  constructor(
+    cep = "",
+    rua = "",
+    bairro = "",
+    cidade = "",
+    uf = "",
+    complemento = ""
+  ) {
+    this.cep = cep;
+    this.rua = rua;
+    this.bairro = bairro;
+    this.cidade = cidade;
+    this.uf = uf;
+    this.complemento = complemento;
+  }
+
+  async buscarEndereco(cep) {
+    try {
+      let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      let dados = await response.json();
+
+      console.log(dados);
+
+      this.cep = dados.cep;
+      this.rua = dados.rua;
+      this.bairro = dados.bairro;
+      this.cidade = dados.cidade;
+      this.uf = dados.uf;
+      this.complemento = dados.complemento || "";
+
+      return this;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
   }
 }
 
-buscarEndereco("01001000");
-console.log("Requisição Enviada");
+class TipoEndereco extends Endereco {
+  constructor(cep, rua, bairro, cidade, uf, complemento, tipo) {
+    super(cep, rua, bairro, cidade, uf, complemento);
+    this.tipo = tipo;
+  }
+
+  exibirEndereco() {
+    return {
+      tipo: this.tipo,
+      cep: this.cep,
+      rua: this.rua,
+      bairro: this.bairro,
+      cidade: this.cidade,
+      uf: this.uf,
+      complemento: this.complemento,
+    };
+  }
+}
+
+document
+  .getElementById("cep")
+  .addEventListener("input", async function (event) {
+    const cep = event.target.value;
+
+    // if (cep.length === 8) {
+    //   console.log("CEP VÁLIDO!");
+    // }
+
+    if (cep.length === 8) {
+      const endereco = new Endereco();
+      const results = await endereco.buscarEndereco(cep);
+
+      console.log(results);
+
+      if (results) {
+        document.getElementById("rua").value = results.rua;
+        document.getElementById("bairro").value = results.bairro;
+        document.getElementById("cidade").value = results.cidade;
+        document.getElementById("uf").value = results.uf;
+        document.getElementById("complemento").value = results.complemento;
+      } else {
+        alert("CEP INVÁLIDO");
+      }
+    }
+  });
